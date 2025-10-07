f2f27bab45234c9aab8bdbba965a4fc770637756
KKShaminda <shamindakaveesha72@gmail.com>
2025-10-07 22:47:29 +0530

    Create admin dashboard

---
+++ joblink-frontend/src/app/admin/admin-dashboard/admin-dashboard.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  imports: [ RouterModule ],
+  templateUrl: './admin-dashboard.html',
+  styleUrl: './admin-dashboard.scss'
+})
+export class AdminDashboard {
+
+}
